72fe979c17d7c27a7922eb582b2030a0756a9bc0
kevinneyer <kevinneyer@gmail.com>
2020-11-30 16:56:45 -0500

    working on lab

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
 function bfs(rootNode, vertices, edges){
 
 }
+
+function findAdjacent(node, vertices, edges){
+    let array = []
+    let answer = []
+    for(let i = 0; i < edges.length; i++){
+        for(let j = 0; j < edges[i].length; j++){
+            if(edges[i][j] === node && j === 0){
+                array.push(edges[i][j + 1])
+            } else if 
+                (edges[i][j] === node && j === 1){
+                    array.push(edges[i][j - 1])
+                }
+        }
+    }
+    for(let k = 0; k < array.length; k++){
+        for(let n = 0; n < vertices.length; n++){
+            if(vertices[n].name === array[k]){
+                answer.push(vertices[n])
+            }
+        }
+    }
+    return answer
+}
+
+function markDistanceAndPredecessor(){
+
+}
+
+function addToQueue(){
+
+}
